18b66720cf04695c6129c146ae78b6ff5f5a7da3
tko-cactus <37374201+tko-cactus@users.noreply.github.com>
2021-09-21 13:10:38 +0900

    overwirte the entrypoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]  // 標準ライブラリを無効化する
+#![no_main]  // コンパイラに通常のエントリポイント(main)を使いたくないことを伝える
+
+use core::panic::PanicInfo;
+
+// OSのエントリポイントを上書き
+#[no_mangle] // no_mangle attribute ... 名前修飾(コンパイラが関数名をユニークにするために色々付け足すこと）を無効化する
+pub extern "C" fn _start() -> ! {   // Cの呼び出し規約を使用するように指定(ほとんどのシステムのデフォルトエントポイント)
+    loop{}
+}
+
+// この関数はPanic時に呼ばれる
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {  // この関数は戻り値を返すべきでない->never型を返す
+    loop {} 
 }
